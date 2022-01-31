5f770ec50b3d08e4df73bdb25862e5441000200d
Jose Vega <jocvegar@gmail.com>
2022-01-31 07:54:56 -0600

    add broadcast

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   # broadcasts
-  # after_create_commit { broadcast_append_later_to('posts') }
+  after_create_commit { broadcast_append_later_to('posts') }
   validates :title, presence: true
 end
