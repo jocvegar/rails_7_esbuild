f772b66a737dcdae487e73357f910378574735b4
Jose Vega <jocvegar@gmail.com>
2022-02-14 17:18:40 -0600

    few updates

--- app/javascript/components/vue.js
+++ app/javascript/components/vue.js
@@ -1,13 +1,13 @@
 import Vue from "vue/dist/vue.js";
 
-new Vue({
-  el: "#vue",
-
-  data: () => ({ message: "Hello Vue.js!" }),
-
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split("").reverse().join("");
+if (document.getElementById("vue")) {
+  new Vue({
+    el: "#vue",
+    data: () => ({ message: "Hello World! I am Vue.js!" }),
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split("").reverse().join("");
+      },
     },
-  },
-});
+  });
+}
