0250fdf9bb9c56bdc624d8cf6f3a762ff221d323
Jose Vega <jocvegar@gmail.com>
2022-09-07 20:31:27 -0600

    fix redis

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_220245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
